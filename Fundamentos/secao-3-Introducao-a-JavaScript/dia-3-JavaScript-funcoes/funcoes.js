9cd0311681fe6117793a3abf669a562affa05ef5
AnaCarolineOliv <ana.caroline.ifg.edu@gmail.com>
2023-05-04 16:17:32 -0300

    adiciona requisito 3

--- Fundamentos/secao-3-Introducao-a-JavaScript/dia-3-JavaScript-funcoes/funcoes.js
+++ Fundamentos/secao-3-Introducao-a-JavaScript/dia-3-JavaScript-funcoes/funcoes.js
@@ -7,3 +7,8 @@ function subtraiValor(saldo) {
     return saldo - 100;
 }
 console.log(subtraiValor(700));
+
+function multiplicaValor(saldo) {
+    return saldo*1.7;
+}
+console.log(multiplicaValor(800));
