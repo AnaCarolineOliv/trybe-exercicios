8486a947aaec621fe832b00deefc5f4e6ffe7fee
AnaCarolineOliv <ana.caroline.ifg.edu@gmail.com>
2023-05-03 22:36:22 -0300

    requisito 4 do exercício de array

--- Fundamentos/secao-3-Introducao-a-JavaScript/dia-2-Js-Array-e-loop-For/agora-a-pratica.js
+++ Fundamentos/secao-3-Introducao-a-JavaScript/dia-2-Js-Array-e-loop-For/agora-a-pratica.js
@@ -3,11 +3,17 @@ let result = 0;
 
 
 for (let i = 0; i < numbers.length; i+= 1) {
-    console.log(numbers[i]);
-    result += numbers[i];
-    
+  console.log(numbers[i]);
+  result += numbers[i];
+  
 }
 console.log('soma:', result);
 
 let media = result/numbers.length;
 console.log(media);
+
+if (media >=20) {
+  console.log('O valor da média aritmética é maior que 20');
+} else {
+  console.log('O valor da média aritmética é menor ou igual a 20');
+}
