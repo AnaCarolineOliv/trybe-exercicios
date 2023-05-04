459c3af05d629e34e368d660c02295ee8d17eee0
AnaCarolineOliv <ana.caroline.ifg.edu@gmail.com>
2023-05-03 22:05:17 -0300

    resolve exercicio 3

--- Fundamentos/secao-3-Introducao-a-JavaScript/dia-2-Js-Array-e-loop-For/agora-a-pratica.js
+++ Fundamentos/secao-3-Introducao-a-JavaScript/dia-2-Js-Array-e-loop-For/agora-a-pratica.js
@@ -1,9 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let result = 0;
 
+
 for (let i = 0; i < numbers.length; i+= 1) {
     console.log(numbers[i]);
     result += numbers[i];
     
 }
 console.log('soma:', result);
+
+let media = result/numbers.length;
+console.log(media);
